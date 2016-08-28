1e2d96c47300bc5a5ce36514abb32cce09f067c0
Zar-rok <zar_rok@live.fr>
2016-08-28 16:48:12 +0200

    Update install_dep.bat

--- install_dep.bat
+++ install_dep.bat
@@ -1,4 +1,22 @@
 @echo off
+setlocal enabledelayedexpansion
+
+for /f "tokens=2 delims= " %%i in ('python -V') do (
+
+	set version=%%i
+	set version=!version: =!
+	set version=!version:.=!	
+	
+	if version LSS 279 (
+		python get-pip.py
+	)
+	
+	if version LSS 340 (
+		python get-pip.py
+	)
+)
+
+python -m pip install -U pip
 
 pip install requests
 pip install beautifulsoup4
